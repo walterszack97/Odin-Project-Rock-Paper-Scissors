c63677546b3d00faa5e603e37a05495a1d46d2ce
zachary w <walterszack97@gmail.com>
2023-01-09 17:44:58 -0500

    Refactor index.html and script.js to change the layout of the ui

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
+/////////MANIPULATE DOM BY CREATING NEW ELEMENTS AND ADDING TEXT WITHIN THE FUNCTIONS///////////////////////
+
+
+const myScore = document.querySelector("#playerScore");
+const theirScore = document.querySelector("#computerScore");
+const gameWinner = document.querySelector("#gameWinner");
+const outcome = document.querySelector("#outcome");
+const computerSelect = document.querySelector("#computerSelect");
+
+const gameText = document.querySelector(".gameText");
+const p = document.createElement('p');
+
+const winner = document.querySelector(".winner");
+const winnerH1 = document.createElement('h1');
+
+
+
 ///////////FUNCTIONS TO PLAY GAME//////////////////////
 
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3)
 
     if (num === 0) {
-        return 'ROCK'
+        computerSelect.textContent = "Computer chose: ROCK";
+        return 'ROCK';
     } else if (num === 1){
-        return 'PAPER'
+        computerSelect.textContent = "Computer chose: PAPER";
+        return 'PAPER';
     } else if (num === 2){
-        return 'SCISSORS'
+        computerSelect.textContent = "Computer chose: SCISSORS";
+        return 'SCISSORS';
     } else {
         console.log('Something went wrong in getComputerChoice');
     }
@@ -18,50 +38,49 @@ function playRound(playerSelection, computerSelection) {
     
 
     if (playerSelection === computerSelection) { 
-        p.innerHTML = "player threw " + playerSelection + " and computer threw " + computerSelection + ". It's a tie!";
-        gameText.appendChild(p);
+        outcome.textContent = "player threw " + playerSelection + " and computer threw " + computerSelection + ". It's a tie!";
 
 
     } else if (playerSelection === 'ROCK') { 
         if (computerSelection === 'SCISSORS') {
             playerScore += 1;
-                     
-            p.innerHTML = "You win! ROCK beats SCISSORS";
-            gameText.appendChild(p);
+            
+            myScore.textContent = "Player Score: " + playerScore;
+            outcome.textContent = "You win! ROCK beats SCISSORS"
             game();
         } else {
             computerScore += 1;
 
-            p.innerHTML = "You lose! PAPER beats ROCK";
-            gameText.appendChild(p);
+            theirScore.textContent = "Computer Score: " + computerScore;
+            outcome.textContent = "You lose! PAPER beats ROCK";
             game();
         }
     } else if (playerSelection === 'PAPER') { 
         if (computerSelection === 'ROCK') {
             playerScore += 1;
 
-            p.innerHTML = "You win! PAPER beats ROCK";
-            gameText.appendChild(p);
+            myScore.textContent = "Player Score: " + playerScore;
+            outcome.textContent = "You win! PAPER beats ROCK";
             game();
         } else {
             computerScore += 1;
 
-            p.innerHTML = "You lose! SCISSORS beats PAPER";
-            gameText.appendChild(p);
+            theirScore.textContent = "Computer Score: " + computerScore;
+            outcome.textContent = "You lose! SCISSORS beats PAPER";
             game();
         }
     } else if (playerSelection === 'SCISSORS') { 
         if (computerSelection === 'PAPER') {
             playerScore += 1;
 
-            p.innerHTML = "You win! SCISSORS beats PAPER";
-            gameText.appendChild(p);
+            myScore.textContent = "Player Score: " + playerScore;
+            outcome.textContent = "You win! SCISSORS beats PAPER";
             game();
         } else {
             computerScore += 1;
 
-            p.innerHTML = "You lose! ROCK beats SCISSORS";
-            gameText.appendChild(p);
+            theirScore.textContent = "Computer Score: " + computerScore;
+            outcome.textContent = "You lose! ROCK beats SCISSORS";
             game();
         }
     }
@@ -73,14 +92,18 @@ function game() {
     
     
     if (playerScore >= 5) {
-        winnerH1.textContent = "Congratulations! You win!";
+        gameWinner.textContent = "Congratulations! You win!";
+
+
         winner.appendChild(winnerH1);
         score.removeChild(scoreP);
         gameText.removeChild(p);
 
         setTimeout(newGame, 5000);
     } else if (computerScore >= 5) {
-        winnerH1.textContent = "YOU LOSE, TRY AGAIN!";
+        gameWinner.textContent = "YOU LOSE, TRY AGAIN!";
+
+
         winner.appendChild(winnerH1);
         score.removeChild(scoreP);
         gameText.removeChild(p);
@@ -102,23 +125,14 @@ let computerScore = 0;
 
 /////////////////////ADDING EVENT HANDLERS TO BUTTONS//////////////////////////
 const rockBtn = document.querySelector('#rockBtn');
-rockBtn.addEventListener('click', function() {playRound('ROCK', getComputerChoice())});
+rockBtn.addEventListener('click', () => {playRound('ROCK', getComputerChoice())});
 
 const paperBtn = document.querySelector('#paperBtn');
-paperBtn.addEventListener('click', function() {playRound('PAPER', getComputerChoice())});
+paperBtn.addEventListener('click', () => {playRound('PAPER', getComputerChoice())});
 
 const scissorsBtn = document.querySelector('#scissorsBtn');
-scissorsBtn.addEventListener('click', function() {playRound('SCISSORS', getComputerChoice())});
+scissorsBtn.addEventListener('click', () => {playRound('SCISSORS', getComputerChoice())});
 
 
 
-/////////MANIPULATE DOM BY CREATING NEW ELEMENTS AND ADDING TEXT WITHIN THE FUNCTIONS///////////////////////
-const score = document.querySelector(".score");
-const scoreP = document.createElement("p");
-
-const gameText = document.querySelector(".gameText");
-const p = document.createElement('p');
-
-const winner = document.querySelector(".winner");
-const winnerH1 = document.createElement('h1');
 
