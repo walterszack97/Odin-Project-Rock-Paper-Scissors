b6b36a66621b6fd15de447025ad394eb3931242f
zachary w <walterszack97@gmail.com>
2023-01-08 23:51:51 -0500

    Change script from entering user input to clicking a button to submit input using event handlers, modify the dom to add divs and output playRound results into the dom

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+///////////FUNCTIONS TO PLAY GAME//////////////////////
+
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3)
 
@@ -14,42 +16,59 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return ("player threw " + playerSelection + " and computer threw " + computerSelection + ". It's a tie!");
+     
+        const div = document.createElement('div');
+        div.innerHTML = "player threw " + playerSelection + " and computer threw " + computerSelection + ". It's a tie!";
+        document.body.appendChild(div);
+
     } else if (playerSelection === 'ROCK') { 
         if (computerSelection === 'SCISSORS') {
             playerScore += 1;
-            return("You win! ROCK beats SCISSORS");
+        
+            const div = document.createElement('div');
+            div.innerHTML = "You win! ROCK beats SCISSORS";
+            document.body.appendChild(div);
         } else {
             computerScore += 1;
-            return("You lose! PAPER beats ROCK");
+
+            const div = document.createElement('div');
+            div.innerHTML = "You lose! PAPER beats ROCK";
+            document.body.appendChild(div);
         }
     } else if (playerSelection === 'PAPER') { 
         if (computerSelection === 'ROCK') {
             playerScore += 1;
-            return("You win! PAPER beats ROCK");
+
+            const div = document.createElement('div');
+            div.innerHTML = "You win! PAPER beats ROCK";
+            document.body.appendChild(div);
         } else {
             computerScore += 1;
-            return("You lose! SCISSORS beats PAPER");
+  
+            const div = document.createElement('div');
+            div.innerHTML = "You lose! SCISSORS beats PAPER";
+            document.body.appendChild(div);
         }
     } else if (playerSelection === 'SCISSORS') { 
         if (computerSelection === 'PAPER') {
             playerScore += 1;
-            return("You win! SCISSORS beats PAPER");
+        
+            const div = document.createElement('div');
+            div.innerHTML = "You win! SCISSORS beats PAPER";
+            document.body.appendChild(div);
         } else {
             computerScore += 1;
-            return("You lose! ROCK beats SCISSORS");
+            
+            const div = document.createElement('div');
+            div.innerHTML = "You lose! ROCK beats SCISSORS";
+            document.body.appendChild(div);
         }
     }
 }
 
 function game() {
    
-    let playerSelection = prompt("Enter Rock, Paper or Scissors: ").toUpperCase();
-
-    while (playerSelection != 'ROCK' && playerSelection != 'SCISSORS' && playerSelection != 'PAPER'){
-        playerSelection = prompt("You must enter Rock, Paper or Scissors: ").toLocaleUpperCase();
-    }
-    const computerSelection = getComputerChoice();
+   
     console.log(playRound(playerSelection, computerSelection));
     console.log("Your score: " + playerScore);
     console.log("Computer score: " + computerScore);
@@ -63,9 +82,19 @@ function game() {
     }
 }
 
-
-
-
 let playerScore = 0;
 let computerScore = 0;
-game();
+
+
+
+/////////////////////ADDING EVENT HANDLERS TO BUTTONS//////////////////////////
+const rockBtn = document.querySelector('#rockBtn');
+rockBtn.addEventListener('click', function() {playRound('ROCK', getComputerChoice())});
+
+const paperBtn = document.querySelector('#paperBtn');
+paperBtn.addEventListener('click', function() {playRound('PAPER', getComputerChoice())});
+
+const scissorsBtn = document.querySelector('#scissorsBtn');
+scissorsBtn.addEventListener('click', function() {playRound('SCISSORS', getComputerChoice())});
+
+
